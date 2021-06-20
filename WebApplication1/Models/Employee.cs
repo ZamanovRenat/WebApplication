63a8c9e862551d2457315eeaba0c0824dc7c3f1e
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-20 13:10:51 +0300

    Модели департаментов и сотрудников

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeSurName { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeePatronymic { get; set; }
+        public string Department { get; set; }
+
+        public string DateOfJoining { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
